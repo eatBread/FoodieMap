fd42ad8a741817503c222ff9224b6a38b83f6f05
LeeJunlong <brettlee@BrettdeMacBook-Pro.local>
2017-09-18 14:34:01 -0700

    nearby search show header

--- public/js/mymap.js
+++ public/js/mymap.js
@@ -1,36 +1,77 @@
-var DEFAULT_ZOOM = 10;
-var GOOGLE_API_KEY = 'AIzaSyAcEOTH2FqnbaDecCEJOyvoIj8bYuRFbR0';
 
 $(function(){
-function initMap(){
-    var position = {
-    	lat: 40.623290,
-    	lng: -74.024127
-    };
-    var map = new google.maps.Map($('#map')[0], {
+    var GOOGLE_API_KEY = 'AIzaSyAcEOTH2FqnbaDecCEJOyvoIj8bYuRFbR0';
+
+    var DEFAULT_ZOOM = 10;
+    var DEFAULT_RADIUS = 500;
+    var DEFAULT_LAT = 40.623290;
+    var DEFAULT_LNG = -74.024127;
+
+    function initMap(){
+       var position = new google.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG);
+
+       var map = new google.maps.Map($('#map')[0], {
         zoom: DEFAULT_ZOOM,
         center: position
-    });
-    var marker = new google.maps.Marker({
-        position: position,
-        map: map
-    });
+       });
 
-    $.ajax({
-        url: '/nearby_search',
-        data: {
-            'location': position.lat + ',' + position.lng,
-            'type': 'restaurant',
-            'key': GOOGLE_API_KEY,
-            'radius': 500
-        },
-        success: function (data) {
-            debugger;
-        },
-        failure: function (data) {
-            debugger;
+       var params = {
+         'location': position,
+         'radius': DEFAULT_RADIUS,
+         'type': restaurant
+       }
+
+       var service = new google.maps.places.PlacesService(map);
+
+       service.nearbySearch(params, function(result, status){
+         if(status == google.maps.places.PlacesServiceStatus.OK){
+            var current_infowindow;
+
+            _.each(results, function(place){
+                var marker = new google.maps.Marker({
+                    position: {
+                        'lat': place.geometry.location.lat(),
+                        'lng': place.geometry.location.lng()
+                    },
+                    map: map
+                });
+
+
+                var infowindow_content = 
+                '<div id = "content">' +
+                '<h1 class = "firstHeading">' + place.name + '</h1>' +
+                '</div>';
+                var infowindow = new google.maps.InfoWindow({
+                    content: infowindow_content
+                });
+
+                marker.addListener('click',function(){
+                    if(current_infowindow) {
+                        current_infowindow.close();
+                    }
+                    infowindow.open(map, marker);
+                    current_infowindow = infowindow;
+                });
+            });
         }
     });
-}
-initMap();
+
+
+       // $.ajax({
+       //  url: '/nearby_search',
+       //  data: {
+       //      'location': position.lat + ',' + position.lng,
+       //      'type': 'restaurant',
+       //      'key': GOOGLE_API_KEY,
+       //      'radius': DEFAULT_RADIUS
+       //  },
+       //  success: function (data) {
+       //      debugger;
+       //  },
+       //  failure: function (data) {
+       //      debugger;
+       //  }
+       //  });
+   }
+   initMap();
 });
